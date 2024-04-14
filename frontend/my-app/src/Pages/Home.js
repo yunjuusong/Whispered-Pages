eaa449c65e5f8583dd65b0cb16c60f3505e444d1
Yunju Song <gpyunju2004@gmail.com>
2024-04-14 00:28:04 -0700

    moving stuff

--- frontend/my-app/src/Pages/Home.js
+++ frontend/my-app/src/Pages/Home.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import Logo from '../Components/LOGO.png';
+import { Link } from 'react-router-dom'; // Import Link from react-router-dom
+import Logo from '../Components/LOGO.png'; // Adjust the path to access the image
 import BookPic from '../Components/book.png';
 import './Home.css';
 
@@ -12,8 +12,8 @@ function Home() {
         <h1>Welcome to My Ebook Reading Platform</h1>
         <p>Start your reading adventure here!</p>
 
-        {/* Corrected Link to direct to "/Book" */}
-        <Link to="/Book">
+        {/* Make the bookpic clickable and direct to book1.js */}
+        <Link to="/Components/book1">
           <img src={BookPic} alt="BookPic" className="bookpic" />
         </Link>
       </div>
