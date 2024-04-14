99afd4676233de312be79cdc8d7a8b3892d0f8f4
Yunju Song <gpyunju2004@gmail.com>
2024-04-13 22:31:33 -0700

    basic design

--- frontend/my-app/src/App.js
+++ frontend/my-app/src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import HomePage from './Pages/Home';
+import NotFoundPage from './Pages/NotFound';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route exact path="/" element={<HomePage />} /> {/* Default route */}
+          <Route path="/Home" element={<HomePage />} />
+          <Route path="*" element={<NotFoundPage />} /> {/* This route will be matched for any other route */}
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
